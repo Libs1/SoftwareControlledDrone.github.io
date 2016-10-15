82e8936bd87a94ff7c26cb2ba9a451025218d7f6
Libs1 <kevinlibdan@Hotmail.com>
2016-10-15 15:49:42 -0400

    main updated

---
+++ softwarecontrolleddrone/src/main/java/com/example/softwarecontrolleddrone/MenuActivity.java
@@ -0,0 +1,55 @@
+/*
+Team Members: Denis Stepanov, Kevin Libdan, Pramit Roy
+Team name: Skynet
+*/
+
+package com.example.softwarecontrolleddrone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    TextView logout;
+    Button controllerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        logout = (TextView)findViewById(R.id.logoutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MenuActivity.this, R.string.logoutToast, Toast.LENGTH_SHORT)
+                        .show();
+
+                Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        controllerButton = (Button)findViewById(R.id.controllerButton);
+        controllerButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(MenuActivity.this, ControllerActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+    }
+}
