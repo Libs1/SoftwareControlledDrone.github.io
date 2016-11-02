5046a1c3fd0c751f8e5248d9c8dab2da369ceb7d
n01077691 <denistepp1@gmail.com>
2016-11-02 00:22:40 -0700

    fix#2
    upload successfull
    
    1. Pulled Kevin-scd version of app with "Remember Me" button
    2. Finished implementing LED button.

---
+++ softwarecontrolleddrone/src/main/java/com/example/softwarecontrolleddrone/ControllerActivity.java
@@ -0,0 +1,170 @@
+/*
+Team Members: Denis Stepanov, Kevin Libdan, Pramit Roy
+Team name: Skynet
+*/
+
+package com.example.softwarecontrolleddrone;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.annotation.BoolRes;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ControllerActivity extends AppCompatActivity {
+    public static final String PREFS = "sharedPreferences";
+    public static Button b;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller);
+
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ControllerActivity.this, PopActivity.class));
+            }
+        });
+
+
+        ImageView upButton = (ImageView)findViewById(R.id.upArrow);
+        upButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                ImageView bluedrone1 = (ImageView)findViewById(R.id.dronePicture2);
+
+                Animation jumpAnimation = AnimationUtils.loadAnimation(ControllerActivity.this, R.anim.drone_anim_up);
+                bluedrone1.startAnimation(jumpAnimation);
+            }
+        });
+
+        ImageView downButton = (ImageView)findViewById(R.id.downArrow);
+        downButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+
+                ImageView bluedrone1 = (ImageView)findViewById(R.id.dronePicture2);
+
+                Animation jumpAnimation = AnimationUtils.loadAnimation(ControllerActivity.this, R.anim.drone_anim_down);
+                bluedrone1.startAnimation(jumpAnimation);
+            }
+        });
+
+        ImageView leftButton = (ImageView)findViewById(R.id.leftArrow);
+        leftButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                ImageView bluedrone1 = (ImageView)findViewById(R.id.dronePicture2);
+
+                Animation jumpAnimation = AnimationUtils.loadAnimation(ControllerActivity.this, R.anim.drone_anim_left);
+                bluedrone1.startAnimation(jumpAnimation);
+            }
+        });
+
+        ImageView rightButton = (ImageView)findViewById(R.id.rightArrow);
+        rightButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                ImageView bluedrone1 = (ImageView)findViewById(R.id.dronePicture2);
+
+                Animation jumpAnimation = AnimationUtils.loadAnimation(ControllerActivity.this, R.anim.drone_anim_right);
+                bluedrone1.startAnimation(jumpAnimation);
+            }
+        });
+
+        ImageView leftRotate = (ImageView)findViewById(R.id.rotateLeft);
+        leftRotate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                ImageView bluedrone1 = (ImageView)findViewById(R.id.dronePicture2);
+
+                Animation jumpAnimation = AnimationUtils.loadAnimation(ControllerActivity.this, R.anim.drone_rotate_left);
+                bluedrone1.startAnimation(jumpAnimation);
+            }
+        });
+
+        ImageView rightRotate = (ImageView)findViewById(R.id.rotateRight);
+        rightRotate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                ImageView bluedrone1 = (ImageView)findViewById(R.id.dronePicture2);
+
+                Animation jumpAnimation = AnimationUtils.loadAnimation(ControllerActivity.this, R.anim.drone_rotate_right);
+                bluedrone1.startAnimation(jumpAnimation);
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()) {
+            case R.id.help:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.ca/"));
+                startActivity(intent);
+                break;
+
+            case R.id.scd:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.litehawk.ca/highroller/"));
+                startActivity(intent);
+                break;
+
+            case R.id.drone:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dronestoronto.com/"));
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle(R.string.dialogMsg)
+                .setMessage(R.string.dialogMsg2)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+}
