0e262d647d91542fa839019bb7dcf05c9e2391b4
Libs1 <kevinlibdan@Hotmail.com>
2016-12-12 17:45:35 -0500

    flightpath button
    Added flightpath button into activity_menu (land),(large) and (xlarge)
    as they were not included in those layouts.

--- softwarecontrolleddrone/src/main/java/com/example/softwarecontrolleddrone/MenuActivity.java
+++ softwarecontrolleddrone/src/main/java/com/example/softwarecontrolleddrone/MenuActivity.java
@@ -18,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
